1f94ca3680d12f0a3e65480b91ba170a8f0d3a10
Shuvkant <weird426344@gmail.com>
2024-12-14 21:43:18 +0545

    adding 14 solution

---
+++ lab_01/14__solution.py
@@ -0,0 +1,21 @@
+"""
+ Write a pithon program to create a class representing a Circle. Include methods to calculate its area
+and perimeter.
+"""
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius=radius
+
+    def area(self):
+        area= (math.pi)*(self.radius)**2
+        print(f"{area:.2f}")
+
+    def perimeter(self):
+        perimeter= (math.pi)*2*(self.radius)
+        print(f"{perimeter:.2f}")
+
+p1=Circle(5)
+p1.area()
+p1.perimeter()
+
--- lab_01/14_solution.py
+++
@@ -1,4 +0,0 @@
-"""
- Write a Python program to create a class representing a Circle. Include methods to calculate its area
-and perimeter.
-"""
